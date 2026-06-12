5acc19bfb95dfad02bf6fe34fb25f3fc8d097d67
malek-chermiti <malek02chermiti@gmail.com>
2026-06-13 00:09:48 +0100

    Set base path for GitHub Pages deployment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/my-app/',  // ← nom de ton repo GitHub
   test: {
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
